19206c778ebc31b5d8ffae2ebd4fb20b85f92986
Verdi Erel Ergün <vergun@gmail.com>
2013-07-06 18:14:22 -0700

    Add custom labels part 1

--- app/modules/charts_helper.js
+++ app/modules/charts_helper.js
@@ -32,11 +32,11 @@ exports.addColorsToGraphData = function(colors, graphData) {
 }
 
 exports.setup = function(req, query) {
-  console.log(moment("06/26/2013"));
-  console.log(moment("06/26/2013").format());
-  console.log(moment("06/26/2013").startOf('day'));
-  console.log(moment("06/26/2013").endOf('day'));
   
+  // days, weeks, months
+  // compare dates first and second distance from one another
+  // if less than 7 days display days
+  // if more than 7 days display months
   
 var query = query.split("&")
   , dates = query[0].replace("date=", "").split('.')
@@ -50,6 +50,65 @@ var query = query.split("&")
   , perPage = 30
   , page = (req.param('page') > 0 ? req.param('page') : 1) - 1
   
+  
+  //** start tests **/
+  
+  Date.prototype.addDays = function(days) {
+      var dat = new Date(this.valueOf())
+      dat.setDate(dat.getDate() + days);
+      return dat;
+  }
+  
+  function getDates(startDate, stopDate) {
+     var dateArray = new Array();
+     var currentDate = startDate;
+     while (currentDate <= stopDate) {
+       dateArray.push(currentDate)
+       currentDate = currentDate.addDays(1);
+     }
+     return dateArray;
+   }
+   
+   var dateArray = getDates(time_range[0], time_range[1]);
+   
+   if ( dateArray.length <= 7 ) {
+     
+     for (i = 0; i < dateArray.length; i ++ ) {
+         dateArray[i] = moment(dateArray[i]).format('dddd');
+     } 
+     
+     
+   } 
+   
+   if ( dateArray.length > 7 && dateArray.length <= 365 ) {
+     
+     for (i = 0; i < dateArray.length; i ++ ) {
+         dateArray[i] = moment(dateArray[i]).format('MMMM');
+     } 
+     
+     dateArray = _.uniq(dateArray);
+     
+   }
+   
+   if ( dateArray.length > 365 ) {
+     
+     for (i = 0; i < dateArray.length; i ++ ) {
+         dateArray[i] = moment(dateArray[i]).format('YYYY');
+     } 
+     
+     dateArray = _.uniq(dateArray);
+     
+     
+   }
+      
+   
+      
+   console.log(dateArray);
+
+   
+   //** end tests **/
+   
+  
   return {
     query: query
   , dates: dates
@@ -61,7 +120,7 @@ var query = query.split("&")
   , simple_charts: [ "Doughnut", "Pie", "PolarArea" ]
   , complex_charts: [ "Bar", "Line", "Radar" ]
   , colors: [ "#F38630", "#E0E4CC", "#69D2E7", "#6AD2E7", "#6A93E7", "#7F6AE7", "#BD6AE7", "#6AE7BD", "#35C2DE", "#1E9FB8", "#E76AD2", "#6AE77F", "#B8381E", "#DE5135", "#E76A93", "#D2E76A", "#E7BD6A", "#E77F6A" ]
-  , labels: [ "January", "February", "March", "April", "May", "June", "July" ]
+  , labels: dateArray
   , user: req.user
   , page: page
   , perPage: perPage
