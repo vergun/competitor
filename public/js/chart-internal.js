b35dd6eda9cb6e93d352b97deb03b9b4216eeda2
Verdi Erel Ergün <vergun@gmail.com>
2013-07-12 15:56:24 -0700

    Basic charts working with new design

--- public/js/chart-internal.js
+++ public/js/chart-internal.js
@@ -36,27 +36,45 @@
     },
      
     // only one selection elements
-    groupRadial: function() {
-      $('.group-radial').bind('click', function() {
-        $(this).siblings().each(function() {
-          $(this).removeClass('selected');
-        })
-        $(this).addClass('selected');
+    // groupRadial: function() {
+    //   $('.group-radial').bind('click', function() {
+    //     $(this).siblings().each(function() {
+    //       $(this).removeClass('selected');
+    //     })
+    //     $(this).addClass('selected');
+    //   })
+    // },
+    
+    
+    groupChart: function() {
+      $('.chart-types').bind('click', function() {
+        var _this = this;
+        $('.chart-types').each(function() {
+          $(this).parent().removeClass('active');
+        });
+        $(_this).parent().addClass('active');
+      })
+    },
+    
+    groupKeywords: function() {
+      $('.update-keyword').bind('click', function() {
+        console.log(this);
+        $(this).toggleClass('active');
       })
     },
     
     // on/off style elements
-    groupToggle: function() {
-      $('.group-toggle').bind('click', function() {
-        $(this).toggleClass('selected');
-      })
-    },
+    // groupToggle: function() {
+    //   $('.group-toggle').bind('click', function() {
+    //     $(this).toggleClass('selected');
+    //   })
+    // },
     
     //create an array from keywords
     getKeywords: function() {
       var keywords = []
-      $('button.update-keyword.selected').each(function() {
-        keywords.push($(this).text());
+      $('.update-keyword.active').each(function() {
+        keywords.push($(this).data('keyword'));
       })
       return keywords;
     },
@@ -88,29 +106,33 @@
     },
     
     showLoading: function() {
-      var mainContent = $('.main-content')
-      mainContent.prepend("<h1 class='loading'><div>Loading...</div></h1>")
+      var loading = '<div data-alert class="alert-box">Loading new data.<a href="#" class="close">&times;</a></div>'
+        , element = $('#footer-region');
+      element.prepend(loading)
     },
     
     removeLoading: function() {
-      var loading = $('.loading')
+      var loading = $('.alert-box')
       loading.remove();
     },
     
-    getData: function (e, context ) {
+    getData: function ( e, context ) {
       
       e.preventDefault();
       
       var dates = $("#from").val() + "." + $("#to").val();
+      console.log(dates);
       
       if (dates.length > 1) dates = "date=" + dates
       if (dates.length == 1) dates = "date="
       
-      var chart = $('.chart-types').children('.selected').data('chart');
+      var chart = $('.chart-types').parent('.active').data('chart');
       chart = "&chart=" + chart;
+      console.log(chart)
       
       var keywords = UI.getKeywords();
       keywords = "&keywords=" + keywords.join(";").replace(/ /g, "_");
+      console.log(keywords)
       
       var since = UI.getSince();
       since = "&since=" + since
@@ -134,7 +156,8 @@
       new Chart(ctx).Pie(eval("(" + document.getElementById("chart-data").value + ")"), { animation: true } );  
     },
     
-    receivedChartData: function(dates, chart, keywords, data) {   
+    receivedChartData: function(dates, chart, keywords, data) { 
+      console.log(data);  
       var since = data.since
         , data = data.chartData
         , chart = chart.split("=")[1]
@@ -200,7 +223,9 @@
   Charts.setupFirstChart();
   UI.setupElements();
   UI.getChart();
-  UI.groupRadial();
-  UI.groupToggle();
+  UI.groupChart();
+  UI.groupKeywords();
+  // UI.groupRadial();
+  // UI.groupToggle();
   
 });
